17f72627353b9cb569c15c505e8cb875d88f29e1
Adi Pramono <info@adipramono.com>
2018-11-28 14:33:55 +0800

    Update remove-rev-slider-metabox.php

--- remove-rev-slider-metabox.php
+++ remove-rev-slider-metabox.php
@@ -1,5 +1,12 @@
-//Remove Toolset Front-end Metabox
-function remove_types_info_box() {
-    return false;
+//Remove Rev Slider Metabox
+if ( is_admin() ) {
+
+	function remove_revolution_slider_meta_boxes() {
+		remove_meta_box( 'mymetabox_revslider_0', 'page', 'normal' );
+		remove_meta_box( 'mymetabox_revslider_0', 'post', 'normal' );
+		remove_meta_box( 'mymetabox_revslider_0', 'yacht', 'normal' );
+	}
+
+	add_action( 'do_meta_boxes', 'remove_revolution_slider_meta_boxes' );
+	
 }
-add_filter( 'types_information_table', 'remove_types_info_box' );
